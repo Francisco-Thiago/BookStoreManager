33631a4708bb233c53c8e6166df78b9a1a340e2f
Francisco Thiago <vieirathiago779@gmail.com>
2022-08-02 15:42:08 -0300

    JPA Class 'Publisher' created

---
+++ src/main/java/com/franciscothiago/bookstoremanager/entity/publisher/Publisher.java
@@ -0,0 +1,24 @@
+package com.franciscothiago.bookstoremanager.entity.publisher;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@Entity
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String code;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate foundationDate;
+}
