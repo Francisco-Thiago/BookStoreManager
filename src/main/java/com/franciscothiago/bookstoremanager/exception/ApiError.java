6f5abcd7a5f85430f69a2e9d55b4c7e8e722afd4
Francisco Thiago <vieirathiago779@gmail.com>
2022-08-06 16:37:30 -0300

    Developed Exception Handler

---
+++ src/main/java/com/franciscothiago/bookstoremanager/exception/ApiError.java
@@ -0,0 +1,29 @@
+package com.franciscothiago.bookstoremanager.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiError {
+
+    private int code;
+
+    private String status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss ")
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    private List<String> errors;
+
+}
