cd52700a5f028996c75a1a156a4be69e7cdce338
Francisco Thiago <vieirathiago779@gmail.com>
2022-08-07 12:50:11 -0300

    author deleted

--- src/main/java/com/franciscothiago/bookstoremanager/entity/author/controller/AuthorControllerDocs.java
+++
@@ -1,7 +0,0 @@
-package com.franciscothiago.bookstoremanager.entity.author.controller;
-
-import io.swagger.annotations.Api;
-
-@Api("Authors Management")
-public interface AuthorControllerDocs {
-}
--- src/main/java/com/franciscothiago/bookstoremanager/entity/author/repository/AuthorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.franciscothiago.bookstoremanager.entity.author.repository;
-
-import com.franciscothiago.bookstoremanager.entity.author.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-
-}
