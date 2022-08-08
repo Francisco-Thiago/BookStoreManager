12412bd808caf65ab1e6512f1e1c8f4cce4c11a2
Francisco Thiago <vieirathiago779@gmail.com>
2022-08-08 02:00:30 -0300

    Created message and errors notify

--- src/main/java/com/franciscothiago/bookstoremanager/entity/user/repository/UserRepository.java
+++ src/main/java/com/franciscothiago/bookstoremanager/entity/user/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.franciscothiago.bookstoremanager.entity.user.repository;
 import com.franciscothiago.bookstoremanager.entity.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEMailOrUsername(String email, String username);
+
 }
