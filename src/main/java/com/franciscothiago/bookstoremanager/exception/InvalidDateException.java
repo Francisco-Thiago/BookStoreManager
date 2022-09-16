d65696ea32168d4279eebf7d3d51510a85b76b48
Francisco Thiago <vieirathiago779@gmail.com>
2022-09-16 13:52:32 -0300

    refact: exceptions now they are in pt-br #68

--- src/main/java/com/franciscothiago/bookstoremanager/exception/InvalidDateException.java
+++ src/main/java/com/franciscothiago/bookstoremanager/exception/InvalidDateException.java
@@ -5,11 +5,11 @@ import java.time.DateTimeException;
 public class InvalidDateException extends DateTimeException {
 
     public InvalidDateException(String date) {
-        super(String.format("The date %s is invalid.", date));
+        super(String.format("A data %s é inválida.", date));
     }
 
     public InvalidDateException(String newExpiration, String newReturn) {
-        super(String.format("Date Expiration %s and/or Return Date %s is/are invalid", newExpiration, newReturn));
+        super(String.format("Data de expiração %s e/ou data de retorno %s é/são inválidas.", newExpiration, newReturn));
     }
 
 }
--- src/main/java/com/franciscothiago/bookstoremanager/exception/PublisherAlreadyExists.java
+++ src/main/java/com/franciscothiago/bookstoremanager/exception/PublisherAlreadyExists.java
@@ -5,7 +5,7 @@ import javax.persistence.EntityExistsException;
 public class PublisherAlreadyExists extends EntityExistsException {
 
     public PublisherAlreadyExists(String name, String code) {
-        super(String.format("Publisher %s or code %s already exists", name, code));
+        super(String.format("Editora %s ou código %s já existe!", name, code));
     }
 
 }
